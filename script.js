ee05742e8881a42b7a8f144b4a4bff14cd7e9871
Shabbir <shabbirhassain42@gmail.com>
2026-01-21 08:50:40 -0800

    new project and new version of portfolio is added

--- script.js
+++ script.js
@@ -7,3 +7,27 @@ document.querySelectorAll("a[href^='#']").forEach(anchor => {
         });
     });
 });
+
+const menuBtn = document.getElementById("menuBtn");
+const navLinks = document.getElementById("navLinks");
+
+window.addEventListener("load", () => {
+    if (window.innerWidth <= 768) {
+        menuBtn.style.display = 'block'
+    }
+});
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("nav-active");
+
+    // Change icon
+    menuBtn.textContent =
+        navLinks.classList.contains("nav-active") ? "✖" : "☰";
+});
+
+// Close sidebar when clicking link
+document.querySelectorAll(".nav-links a").forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("nav-active");
+        menuBtn.textContent = "☰";
+    });
+});
